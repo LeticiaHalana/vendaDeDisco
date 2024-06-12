aae8d5e5d1daa739b85411a77a7a7da1ce2fbe53
Leticia Halana Ribeiro de Sousa <leticia.halana.tr@gmail.com>
2024-06-12 15:20:44 -0300

    feat: criação das categorias e seu dark- mode

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,15 @@
-import { getArray, setArray, categories } from "./productsData";
+import { getArray, setArray, categories } from "./productsData.js";
+
+const categorys = document.querySelector(".categorys");
+
+function renderGeneros(categories) {
+    categories.forEach((categoria) => {
+        const botao = document.createElement('button');
+        botao.classList.add("buttons_category");
+        botao.textContent = categoria;
+        categorys.appendChild(botao);
+    });
+
+}
+renderGeneros(categories);
+
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,21 @@
-/* Desenvolva sua lógica aqui ... */
+const button = document.querySelector(".theme");
+const body = document.querySelector('body');
+const title = document.querySelector(".title");
+const title_category = document.querySelector(".title_categorys");
+
+
+const buttons_category = document.querySelectorAll(".buttons_category");
+console.log(buttons_category);
+
+
+button.addEventListener("click", () => {
+    button.classList.toggle("dark-mode-button");
+    body.classList.toggle("dark-mode");
+    title.classList.toggle("dark-mode-title");
+    title_category.classList.toggle("title_categorys_dark");
+
+    buttons_category.forEach((e) => {
+        e.classList.toggle("button_category_dark");
+    })
+
+});
